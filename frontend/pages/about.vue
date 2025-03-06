66a06f93b426ce0cc469ac013d734ad308b8387c
Vasil <vasilverdouw@gmail.com>
2025-03-06 09:13:07 +0100

    ci(testing): add testing pages for ci deployment

---
+++ frontend/pages/about.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div>
+        <h1>About</h1>
+        <p>About page content</p>
+        <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores harum ratione optio ea! Ducimus iusto
+            laborum, exercitationem rem, qui at asperiores obcaecati quas non nostrum laboriosam iure tempora suscipit?
+            Ipsum!
+        </p>
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ frontend/pages/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div>
+    <h1>Home</h1>
+    <p>Home page content</p>
+</div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
