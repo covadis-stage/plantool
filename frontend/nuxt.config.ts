bfd0c17f85349ab81a86754267651ba034c5e1e6
Vasil <vasilverdouw@gmail.com>
2025-03-06 14:48:28 +0100

    fix: update output directory configuration in nuxt.config.ts

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,11 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  
   nitro: {
     preset: "azure",
     output: {
-      serverDir: "../.output/server",
+      dir: "../.output"
     }
   }
 })
