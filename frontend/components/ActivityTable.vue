07c97d8225212b392ffee51e238e3a05c856a014
Vasil <vasilverdouw@gmail.com>
2025-04-01 17:30:58 +0200

    feat(project-table): restructure files to later support virtual scrolling

--- frontend/components/ActivityTable.vue
+++ frontend/components/ActivityTable.vue
@@ -1,73 +1,37 @@
 <script setup lang="ts">
-import type { TreeNode } from 'primevue/treenode';
-import type { Project } from '~/types/Project';
-
 const projectStore = useProjectStore();
-const treeNodes = ref<TreeNode[]>([]);
-const loadingMethod = ref<"mask" | "icon">("mask");
-
-const onNodeExpand = async (node: TreeNode) => {
-    if (node.data && node.children) return;
-    if (!node.data) return;
-    node.loading = true;
-
-    const project = node.data as Project;
-    const activities = await projectStore.getActivitiesForProject(project.key);
-    const activitiesNode = {
-        key: `${project.key}-activities`,
-        label: project.customer,
-        data: activities,
-        leaf: true,
-    } as TreeNode;
-
-    // Add the activities in 1 node so the component can decide how to display them
-    node.children = [activitiesNode];
-    node.loading = false;
-}
 
 onMounted(() => {
     (async () => {
         await projectStore.getProjects();
-        loadingMethod.value = "icon";
     })();
 });
 
-watchEffect(() => {
-    treeNodes.value = projectStore.projects.map((project) => {
-        return {
-            key: project.key,
-            label: project.customer,
-            data: project,
-            leaf: false,
-        } as TreeNode;
-    });
-});
 </script>
 
 <template>
-    <Tree
-        :value="treeNodes"
-        @node-expand="onNodeExpand"
-        :loading="projectStore.loading"
-        :loading-mode="loadingMethod"
-        class="activity-table"
+    <div class="loading" v-if="projectStore.loading">
+        <i class="pi pi-spin pi-spinner"></i>
+    </div>
+    <ProjectData
+        v-for="project in projectStore.projects"
+        :key="project.key"
+        :project="project"
     >
-        <template #default="{ node }">
-            <ProjectOrActivityTreeNode :treeNode="node" />
-        </template>
-    </Tree>
+    </ProjectData>
 </template>
 
-<style lang="scss">
-.activity-table {
-    .p-tree-node-content {
-        display: flex;
-    }
-
-    .p-tree-node-content .p-tree-node-label {
-        flex: 1;
-    }
+<style lang="scss" scoped>
+.loading {
+    position: fixed;
+    top: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100dvw;
+    height: 100dvh;
+}
+i {
+    font-size: 2rem;
 }
 </style>
-
-<style lang="scss" scoped></style>
--- frontend/components/ProjectData.vue
+++ frontend/components/ProjectData.vue
@@ -1,16 +1,45 @@
 <script setup lang="ts">
 import type { Project } from '~/types/Project';
 
-defineProps<{
+const props = defineProps<{
     project: Project
 }>()
+
+const projectStore = useProjectStore();
+const isOpen = ref<boolean | null>(false);
+
+const toggleOpen = () => {
+    if (isOpen.value === null) return;
+    if (isOpen.value === false) {
+        open();
+    } else if (isOpen.value === true) {
+        close();
+    }
+}
+
+const open = async () => {
+    isOpen.value = null;
+    await projectStore.getActivitiesForProject(props.project.key);
+    isOpen.value = true;
+}
+
+const close = () => {
+    isOpen.value = false;
+}
+
 </script>
 
 <template>
-    <div class="project-data">
-        <p class="customer">{{ project.customer }}</p>
+    <div class="project-data" @click="toggleOpen">
+        <div class="left-side">
+            <i v-if="isOpen === false" class="pi pi-chevron-right"></i>
+            <i v-else-if="isOpen === true" class="pi pi-chevron-down"></i>
+            <i v-else class="pi pi-spin pi-spinner"></i>
+            <p class="customer">{{ project.customer }}</p>
+        </div>
         <p>{{ project.key }}</p>
     </div>
+    <ActivityData v-if="isOpen" :activities="project.activities" />
 </template>
 
 <style lang="scss" scoped>
@@ -20,14 +49,36 @@ defineProps<{
     flex-direction: row;
     justify-content: space-between;
     align-items: center;
+    border-radius: 8px;
+    padding: 16px 16px;
+    user-select: none;
+
+    .left-side {
+        display: flex;
+        align-items: center;
+        gap: 16px;
+    }
 
     .customer {
         font-weight: bold;
         font-size: 1.2rem;
     }
 
-    p {
-        margin: 12px 0px;
+    i {
+        color: var(--p-neutral-400);
     }
+
+    p {
+        margin: 0;
+    }
+}
+
+.project-data:hover {
+    background-color: var(--p-neutral-100);
+    cursor: pointer;
+}
+
+.project-data:active {
+    background-color: var(--p-neutral-200);
 }
 </style>
