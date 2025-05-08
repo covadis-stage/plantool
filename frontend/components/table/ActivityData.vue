3f6e39dbe9671589a06738bbb65f3c1258ab0f8e
Vasil <vasilverdouw@gmail.com>
2025-05-08 13:57:42 +0200

    chore(table ui): smal refinement to prepare for adding comments

--- frontend/components/table/ActivityData.vue
+++ frontend/components/table/ActivityData.vue
@@ -51,7 +51,7 @@ const getClassName = (string: string | undefined) => {
 const getActivityClasses = (activity: ProjectActivity) => {
     let classes = [
         getClassName(activity.network),
-        getClassName('KEY:'+activity.key),
+        getClassName('KEY:' + activity.key),
     ];
 
     return classes.join(' ');
@@ -59,7 +59,7 @@ const getActivityClasses = (activity: ProjectActivity) => {
 
 onMounted(() => {
     props.activities.forEach((activity) => {
-        const element = document.querySelector<HTMLElement>(`.${getClassName('KEY:'+activity.key)}`);
+        const element = document.querySelector<HTMLElement>(`.${getClassName('KEY:' + activity.key)}`);
         if (element) element.addEventListener('mouseenter', () => {
             bulkStore.hoverActivity(activity);
         })
@@ -70,7 +70,6 @@ onMounted(() => {
 <template>
     <DataTable
         :value="activities"
-        removable-sort
         striped-rows
         :row-class="(activity) => getActivityClasses(activity)"
         scrollable
@@ -80,12 +79,27 @@ onMounted(() => {
         :sort-order="-1"
         row-group-mode="subheader"
         group-rows-by="network"
-        :class="{'disable-selection': bulkStore.isSelecting}"
+        :class="{ 'disable-selection': bulkStore.isSelecting }"
     >
-        <Column field="network" header="Network"></Column>
-        <Column field="workBreakdownStructure" header="WBS" style="width: 210px"></Column>
-        <Column field="id" header="Activity" style="width: 80px"></Column>
-        <Column field="latestStartDate" header="Latest Start Date" style="min-width: 180px" >
+        <Column
+            field="network"
+            header="Network"
+        ></Column>
+        <Column
+            field="workBreakdownStructure"
+            header="WBS"
+            style="min-width: 200px"
+        ></Column>
+        <Column
+            field="id"
+            header="Activity"
+            style="min-width: 90px"
+        ></Column>
+        <Column
+            field="latestStartDate"
+            header="Latest Start Date"
+            style="min-width: 180px"
+        >
             <template #body="slotProps">
                 <DateAssignment
                     :activity="slotProps.data"
@@ -95,7 +109,11 @@ onMounted(() => {
                 ></DateAssignment>
             </template>
         </Column>
-        <Column field="latestFinishDate" header="Latest Finish Date" style="min-width: 180px">
+        <Column
+            field="latestFinishDate"
+            header="Latest Finish Date"
+            style="min-width: 180px"
+        >
             <template #body="slotProps">
                 <DateAssignment
                     :activity="slotProps.data"
@@ -105,30 +123,56 @@ onMounted(() => {
                 ></DateAssignment>
             </template>
         </Column>
-        <Column field="originalFinishDate" header="Original Finish Date" style="min-width: 120px">
+        <Column
+            field="originalFinishDate"
+            header="Original Finish Date"
+            style="min-width: 120px"
+        >
             <template #body="slotProps">
                 {{ formatDate(slotProps.data.originalFinishDate) }}
             </template>
         </Column>
-        <Column field="timeEstimated" header="Time Estimated" style="min-width: 130px">
+        <Column
+            field="timeEstimated"
+            header="Time Estimated"
+            style="min-width: 130px"
+        >
             <template #body="slotProps">
-               <ReadableTimeSpan :time-span="slotProps.data.timeEstimated"></ReadableTimeSpan>
+                <ReadableTimeSpan :time-span="slotProps.data.timeEstimated"></ReadableTimeSpan>
             </template>
         </Column>
-        <Column field="timeSpent" header="Time Spent" style="min-width: 130px">
+        <Column
+            field="timeSpent"
+            header="Time Spent"
+            style="min-width: 130px"
+        >
             <template #body="slotProps">
                 <ReadableTimeSpan :time-span="slotProps.data.timeSpent"></ReadableTimeSpan>
             </template>
         </Column>
-        <Column field="teamLeader" header="Team Leader" style="width: 200px"></Column>
-        <Column field="workCenter.key" header="Work Center" style="width: 240px">
+        <Column
+            field="teamLeader"
+            header="Team Leader"
+            style="min-width: 200px"
+        ></Column>
+        <Column
+            field="workCenter.key"
+            header="Work Center"
+        >
             <template #body="slotProps">
-                <p v-tooltip.top="formatWorkCenter(slotProps.data.workCenter)" style="overflow: hidden; text-overflow: ellipsis; white-space: nowrap;">
+                <p
+                    v-tooltip.top="formatWorkCenter(slotProps.data.workCenter)"
+                    style="overflow: hidden; text-overflow: ellipsis; white-space: nowrap;"
+                >
                     {{ slotProps.data.workCenter.readableName ?? slotProps.data.workCenter.key }}
                 </p>
             </template>
         </Column>
-        <Column field="activityType.key" header="Activity Type" style="width: 90px">
+        <Column
+            field="activityType.key"
+            header="Activity Type"
+            style="min-width: 90px"
+        >
             <template #body="slotProps">
                 <div class="text-with-icon">
                     <p>{{ slotProps.data.activityType.key }}</p>
@@ -139,7 +183,11 @@ onMounted(() => {
                 </div>
             </template>
         </Column>
-        <Column field="delegator" header="Delegator" style="width: 120px">
+        <Column
+            field="delegator"
+            header="Delegator"
+            style="min-width: 260px"
+        >
             <template #body="slotProps">
                 <EngineerAssignment
                     :activity="slotProps.data"
@@ -149,7 +197,11 @@ onMounted(() => {
                 </EngineerAssignment>
             </template>
         </Column>
-        <Column field="engineer" header="Engineer" style="width: 120px">
+        <Column
+            field="engineer"
+            header="Engineer"
+            style="min-width: 260px"
+        >
             <template #body="slotProps">
                 <EngineerAssignment
                     :activity="slotProps.data"
@@ -160,13 +212,20 @@ onMounted(() => {
             </template>
         </Column>
         <template #groupheader="slotProps">
-            <Button
-                :label="slotProps.data.network"
-                variant="outlined"
-                icon-pos="right"
-                icon="pi pi-chevron-down"
-                @click="openNetworkSelect($event, slotProps.data.network)"
-            ></Button>
+            <div class="group-header-wrapper">
+                <div class="network">
+                    <p class="network-name">Network</p>
+                    <Button
+                        :label="slotProps.data.network"
+                        variant="outlined"
+                        icon-pos="right"
+                        icon="pi pi-chevron-down"
+                        severity="secondary"
+                        size="small"
+                        @click="openNetworkSelect($event, slotProps.data.network)"
+                    ></Button>
+                </div>
+            </div>
         </template>
     </DataTable>
     <Popover ref="popover">
@@ -207,5 +266,22 @@ onMounted(() => {
 
 .disable-selection {
     user-select: none;
+}
+
+.group-header-wrapper {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    .network .network-name {
+        font-size: 0.7rem;
+        color: var(--p-gray-400);
+        font-weight: 700;
+        margin-bottom: 0.2rem;
+        margin-left: 0.25rem;
+        text-transform: uppercase;
+        letter-spacing: 1px;
+    }
+
 }
 </style>
